7c4062861fbf6bb9293902def64df117ba719397
Stefan Teunissen <stefanteunissen1@gmail.com>
2021-03-08 00:57:37 +0100

    Print button display fix

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -3,11 +3,12 @@
     <h2>{{$recipe->title}}</h2>
     <div class="no-print">
         <a href="javascript:void(0);" id="print-button" onclick="window.print();">Print recipe</a>
-        <script>document.getElementById("print-button").style.display='inline';</script>
         @if(\Illuminate\Support\Facades\Auth::check() && \Illuminate\Support\Facades\Auth::id()==$recipe->poster_id)
-            - <a href="{{route("recipes.edit", $recipe->id)}}">Edit</a> -
+            <span id="print-button-support"> - </span><a href="{{route("recipes.edit", $recipe->id)}}">Edit</a> -
             <a href="{{route("recipes.confirmdestroy", $recipe->id)}}">Delete</a>
+            <script>document.getElementById("print-button-support").style.display='inline';</script>
         @endif
+        <script>document.getElementById("print-button").style.display='inline';</script>
     </div>
     <p>{{$recipe->time}}</p>
     <h3>Requirements</h3>

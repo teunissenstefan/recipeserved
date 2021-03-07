b516948487e92020dabbb6242fd1aac3063844a6
Stefan Teunissen <stefanteunissen1@gmail.com>
2021-03-08 00:36:50 +0100

    Only display the recipe in the print view

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,15 +1,17 @@
-<hr/>
-@guest
-<a href="{{route("login")}}">Login</a> -
-<a href="{{route("register")}}">Register</a> -
-@else
-<a href="{{route("dashboard")}}">Dashboard</a> -
-<form method="POST" style="display:inline" action="{{ route('logout') }}">
-    @csrf
-    <input type="submit" value="Log out" class="a-button">
-</form>-
-@endguest
-<a href="{{route("recipes.create")}}">Add recipe</a>
-<div style="float:right;color:gray">
-    {{$totalCountRecipes}} recipes | {{$totalCountUsers}} users
+<div class="no-print">
+    <hr/>
+    @guest
+        <a href="{{route("login")}}">Login</a> -
+        <a href="{{route("register")}}">Register</a> -
+    @else
+        <a href="{{route("dashboard")}}">Dashboard</a> -
+        <form method="POST" style="display:inline" action="{{ route('logout') }}">
+            @csrf
+            <input type="submit" value="Log out" class="a-button">
+        </form>-
+    @endguest
+    <a href="{{route("recipes.create")}}">Add recipe</a>
+    <div style="float:right;color:gray">
+        {{$totalCountRecipes}} recipes | {{$totalCountUsers}} users
+    </div>
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,11 @@
         @include("includes.header")
     </head>
     <body>
-        <a href="{{route("home")}}"><h1>{{env("APP_NAME")}}</h1></a>
-        <small>The recipe site without bloat.</small>
-        <hr/>
+        <div class="no-print">
+            <a href="{{route("home")}}"><h1>{{env("APP_NAME")}}</h1></a>
+            <small>The recipe site without bloat.</small>
+            <hr/>
+        </div>
         @yield("content")
         @include("includes.footer")
     </body>

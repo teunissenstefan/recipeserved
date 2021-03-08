5b96157eb5cb3aace5680f138006f326bf96d69f
Stefan Teunissen <stefanteunissen1@gmail.com>
2021-03-08 17:51:40 +0100

    Donate button

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,7 +10,8 @@
             <input type="submit" value="Log out" class="a-button">
         </form>-
     @endguest
-    <a href="{{route("recipes.create")}}">Add recipe</a>
+    <a href="{{route("recipes.create")}}">Add recipe</a> -
+    <a href="https://www.paypal.com/donate?hosted_button_id=UE89CGJRT3A32" target="_blank">Donate</a>
     <div style="float:right;color:gray">
         {{$totalCountRecipes}} recipes | {{$totalCountUsers}} users
     </div>
